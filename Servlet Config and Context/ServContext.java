5034ce526f4dd63153f5a6d6a7d4058827d3d7e8
Moumita0710 <nmoumitaprasad@gmail.com>
2024-12-20 18:15:33 +0530

    Create ServContext.java

---
+++ Servlet Config and Context/ServContext.java
@@ -0,0 +1,36 @@
+package MyPack;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServContext
+ */
+@WebServlet("/ServContext")
+public class ServContext extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+   
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out=response.getWriter();
+		ServletContext context=getServletContext();
+		out.println("Hello "+context.getInitParameter("uname"));
+		out.println("Please check your mail. Mail address is "+context.getInitParameter("email"));
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
