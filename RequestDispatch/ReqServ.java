dbaab0c2b2a58bbbbe45e4f282287ab7cd0e373a
Moumita0710 <nmoumitaprasad@gmail.com>
2024-12-23 18:45:55 +0530

    Create ReqServ.java

---
+++ RequestDispatch/ReqServ.java
@@ -0,0 +1,34 @@
+package MyPack;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ReqServ2
+ */
+@WebServlet("/ReqServ")
+public class ReqServ extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out=response.getWriter();
+		out.println("In Link 2");
+		out.println("<b>"+request.getParameter("fname")+" your ID is student 677788");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
