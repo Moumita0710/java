4ab6ecd3c2c8f658e6afeaef18821e251b4927d0
Moumita0710 <nmoumitaprasad@gmail.com>
2024-12-27 17:38:57 +0530

    Add files via upload

--- Attribute/Attribute.java
+++ Attribute/Attribute.java
@@ -9,35 +9,37 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
- * Servlet implementation class AllAttribute
+ * Servlet implementation class Attribute
  */
-@WebServlet("/AllAttribute")
-public class AllAttribute extends HttpServlet {
+@WebServlet("/Attribute")
+public class Attribute extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    
+   
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.setContentType("text/html");
 		PrintWriter out=response.getWriter();
+		response.setContentType("text/html");
 		String fname=request.getParameter("fname");
 		String lname=request.getParameter("lname");
-		ServletContext con=getServletContext();
-		con.setAttribute("Application", lname);
-		if(fname.equals("Sachin"))
-		{
-			request.getRequestDispatcher("ShowAttribute").forward(request,response);
-		}
-		else
-		{
-			out.println("Please type name other than Sachin");
-			request.getRequestDispatcher("AllAttribute.html").include(request,response);
-		}
-		out.println("Please click below link for next servlet");
-		out.println("<br><a href='ShowAttribute'>Next</a>");
-		
+		String course=request.getParameter("course");
+		//------APPLICATION--------
+		ServletContext con=request.getServletContext();
+		con.setAttribute("first_name",fname);
+		//------SESSION------
+		HttpSession ses=request.getSession();
+		ses.setAttribute("course", course);
+		//-------REQUEST--------
+		request.setAttribute("last_name", lname);
+		out.println("<h2>visit below link to display attribute.</h2>");
+		out.println("<a href='ShowAttribute'>Show Attribute</a>");
+		request.getRequestDispatcher("ShowAttribute").forward(request, response);
 	}
 
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
--- Attribute/ShowAttribute.java
+++ Attribute/ShowAttribute.java
@@ -3,7 +3,6 @@ package MyPack;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,33 +10,24 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class AllAttribute
+ * Servlet implementation class ShowAttribute
  */
-@WebServlet("/AllAttribute")
-public class AllAttribute extends HttpServlet {
+@WebServlet("/ShowAttribute")
+public class ShowAttribute extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    
+       
+   
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.setContentType("text/html");
 		PrintWriter out=response.getWriter();
 		String fname=request.getParameter("fname");
-		String lname=request.getParameter("lname");
-		ServletContext con=getServletContext();
-		con.setAttribute("Application", lname);
-		if(fname.equals("Sachin"))
-		{
-			request.getRequestDispatcher("ShowAttribute").forward(request,response);
-		}
-		else
-		{
-			out.println("Please type name other than Sachin");
-			request.getRequestDispatcher("AllAttribute.html").include(request,response);
-		}
-		out.println("Please click below link for next servlet");
-		out.println("<br><a href='ShowAttribute'>Next</a>");
-		
+		out.println("Fname="+fname);
+		String lname=(String)getServletContext().getAttribute("Application");
+		out.println("Lname="+lname);
 	}
 
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
