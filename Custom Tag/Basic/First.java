596cfb22dbcce8a459f2caedf0c96f56a4cda90d
Moumita0710 <nmoumitaprasad@gmail.com>
2025-02-05 18:45:49 +0530

    Add files via upload

--- Custom Tag/Basic/First.java
+++ Custom Tag/Basic/First.java
@@ -1,1 +1,24 @@
+package beans;
 
+import java.io.IOException;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.tagext.TagSupport;
+
+public class First extends TagSupport{
+	public First() {
+		
+	}
+	public int doStartTag() throws JspException{
+		JspWriter out=pageContext.getOut();
+		try {
+			out.println("<h1>Testing cutom tag </h1>");
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+		return SKIP_BODY;
+		
+	}
+
+}
